1236ae99e4813e6b06df9330582c99f5716d942d
Spinetti, Dominick <dominick.spinetti@optum.com>
2022-11-09 13:59:22 -0700

    End of Section one 'Interaction with Web Elements'

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -51,26 +51,33 @@ describe("Our first suite", () => {
     cy.contains('[status="warning"]', "Sign in");
 
     cy.get("#inputEmail3")
-    .parents("form")
-    .find("button")
-    .should('contain', 'Sign in')
-    .parents('form')
-    .find('[class="custom-checkbox"]').click()
+      .parents("form")
+      .find("button")
+      .should("contain", "Sign in")
+      .parents("form")
+      .find('[class="custom-checkbox"]')
+      .click();
 
-    cy.contains('nb-card', 'Horizontal form').find('[type="email"]')
-
+    cy.contains("nb-card", "Horizontal form").find('[type="email"]');
   });
 
-  it('then and wrap methods', () => {
-
+  it("then and wrap methods", () => {
     cy.visit("/");
     cy.contains("Forms").click();
     cy.contains("Form Layouts").click();
 
-    cy.contains('nb-card', 'Using the Grid').find('[for="inputEmail1"]').should('contain', 'Email')
-    cy.contains('nb-card', 'Using the Grid').find('[for="inputPassword2"]').should('contain', 'Password')
-    cy.contains('nb-card', 'Basic form').find('[for="exampleInputEmail1"]').should('contain', 'Email address')
-    cy.contains('nb-card', 'Basic form').find('[for="exampleInputPassword1"]').should('contain', 'Password')
+    cy.contains("nb-card", "Using the Grid")
+      .find('[for="inputEmail1"]')
+      .should("contain", "Email");
+    cy.contains("nb-card", "Using the Grid")
+      .find('[for="inputPassword2"]')
+      .should("contain", "Password");
+    cy.contains("nb-card", "Basic form")
+      .find('[for="exampleInputEmail1"]')
+      .should("contain", "Email address");
+    cy.contains("nb-card", "Basic form")
+      .find('[for="exampleInputPassword1"]')
+      .should("contain", "Password");
 
     //tests in selenium style
     // const firstForm = cy.contains('nb-card', 'Using the Grid')
@@ -78,134 +85,137 @@ describe("Our first suite", () => {
 
     // firstForm.find('[for="inputEmail1"]').should('contain', 'Email')
     // //this one fails
-    // firstForm.find('[for="inputPassword2"]').should('contain', 'Password') 
+    // firstForm.find('[for="inputPassword2"]').should('contain', 'Password')
 
     // secondForm.find('[for="exampleInputEmail1"]').should('contain', 'Email address')
     // secondForm.find('[for="exampleInputPassword1"]').should('contain', 'Password')
 
     //-- Cypress style
-    cy.contains('nb-card', 'Using the Grid').then( firstForm => {
-        const emailLabelFirst = firstForm.find('[for="inputEmail1"]').text()
-        const passwordLabelFirst = firstForm.find('[for="inputPassword2"]').text()
+    cy.contains("nb-card", "Using the Grid").then((firstForm) => {
+      const emailLabelFirst = firstForm.find('[for="inputEmail1"]').text();
+      const passwordLabelFirst = firstForm
+        .find('[for="inputPassword2"]')
+        .text();
 
-        expect(emailLabelFirst).to.equal('Email')
-        expect(passwordLabelFirst).to.equal('Password')
+      expect(emailLabelFirst).to.equal("Email");
+      expect(passwordLabelFirst).to.equal("Password");
 
-        cy.contains('nb-card', 'Basic form').then( secondForm => {
-            const passwordLabelSecond = secondForm.find('[for="exampleInputPassword1"]').text()
-            expect(passwordLabelFirst).to.equal(passwordLabelSecond)
+      cy.contains("nb-card", "Basic form").then((secondForm) => {
+        const passwordLabelSecond = secondForm
+          .find('[for="exampleInputPassword1"]')
+          .text();
+        expect(passwordLabelFirst).to.equal(passwordLabelSecond);
 
-            cy.wrap(secondForm).find('[for="exampleInputPassword1"]').should('contain', 'Password')
-        })
-        
-    })
+        cy.wrap(secondForm)
+          .find('[for="exampleInputPassword1"]')
+          .should("contain", "Password");
+      });
+    });
+  });
 
-  })
-
-  it('invoke command', () => {
+  it("invoke command", () => {
     cy.visit("/");
     cy.contains("Forms").click();
     cy.contains("Form Layouts").click();
 
     //1
-    cy.get('[for="exampleInputEmail1"]').should('contain', 'Email address')
+    cy.get('[for="exampleInputEmail1"]').should("contain", "Email address");
 
     //2
-    cy.get('[for="exampleInputEmail1"]').then( label => {
-        expect(label.text()).to.equal('Email address')
-    })
+    cy.get('[for="exampleInputEmail1"]').then((label) => {
+      expect(label.text()).to.equal("Email address");
+    });
 
     //3
-    cy.get('[for="exampleInputEmail1"]').invoke('text').then( text => {
-        expect(text).to.equal('Email address')
-    })
+    cy.get('[for="exampleInputEmail1"]')
+      .invoke("text")
+      .then((text) => {
+        expect(text).to.equal("Email address");
+      });
 
     //checkbox example with invoke
-    cy.contains('nb-card', 'Basic form')
-        .find('nb-checkbox')
-        .click()
-        .find('.custom-checkbox')
-        .invoke('attr', 'class')
-        // .should('contain', 'checked')
-        .then(classValue => {
-            expect(classValue).to.contain('checked')
-        })
-    
-  })
+    cy.contains("nb-card", "Basic form")
+      .find("nb-checkbox")
+      .click()
+      .find(".custom-checkbox")
+      .invoke("attr", "class")
+      // .should('contain', 'checked')
+      .then((classValue) => {
+        expect(classValue).to.contain("checked");
+      });
+  });
 
-  it('Assert property with datepicker', () => {
-
-
+  it("Assert property with datepicker", () => {
     function selectDayFromCurrent(day) {
-        let date = new Date()
-        date.setDate(date.getDate() + day)
-        let futureDay = date.getDate()
-        let futureMonth = date.toLocaleString('default', {month: 'short'})  
-        let dateAssert = futureMonth + ' ' + futureDay + ', ' + date.getFullYear()
+      let date = new Date();
+      date.setDate(date.getDate() + day);
+      let futureDay = date.getDate();
+      let futureMonth = date.toLocaleString("default", { month: "short" });
+      let dateAssert =
+        futureMonth + " " + futureDay + ", " + date.getFullYear();
 
-        cy.get('nb-calendar-navigation').invoke('attr', 'ng-reflect-date').then( dateAtribute => {
-            if(!dateAtribute.includes(futureMonth)) {
-                cy.get('[data-name="chevron-right"]').click()
-                selectDayFromCurrent(day)                    
-            } else {
-                cy.get('nb-calendar-day-picker [class="day-cell ng-star-inserted"]').contains(futureDay).click()
-            }
-            
-        })
-        return dateAssert
+      cy.get("nb-calendar-navigation")
+        .invoke("attr", "ng-reflect-date")
+        .then((dateAtribute) => {
+          if (!dateAtribute.includes(futureMonth)) {
+            cy.get('[data-name="chevron-right"]').click();
+            selectDayFromCurrent(day);
+          } else {
+            cy.get('nb-calendar-day-picker [class="day-cell ng-star-inserted"]')
+              .contains(futureDay)
+              .click();
+          }
+        });
+      return dateAssert;
     }
 
     cy.visit("/");
     cy.contains("Forms").click();
     cy.contains("Datepicker").click();
 
-    cy.contains('nb-card', 'Common Datepicker').find('input').then( input => {
-        cy.wrap(input).click()
-        let dateAssert = selectDayFromCurrent(2)
-        cy.wrap(input).invoke('prop', 'value').should('contain', dateAssert)
-        
-    })
-  })
+    cy.contains("nb-card", "Common Datepicker")
+      .find("input")
+      .then((input) => {
+        cy.wrap(input).click();
+        let dateAssert = selectDayFromCurrent(2);
+        cy.wrap(input).invoke("prop", "value").should("contain", dateAssert);
+      });
+  });
 
-  it('radio button', () => {
+  it("radio button", () => {
     cy.visit("/");
     cy.contains("Forms").click();
     cy.contains("Form Layouts").click();
 
-    cy.contains('nb-card', 'Using the Grid').find('[type="radio"]').then( radioButtons => {
+    cy.contains("nb-card", "Using the Grid")
+      .find('[type="radio"]')
+      .then((radioButtons) => {
         cy.wrap(radioButtons)
-            .first()
-            .check({force: true})
-            .should('be.checked')
+          .first()
+          .check({ force: true })
+          .should("be.checked");
 
-        cy.wrap(radioButtons)
-            .eq(1)
-            .check({force: true})
+        cy.wrap(radioButtons).eq(1).check({ force: true });
 
-        cy.wrap(radioButtons)
-            .first()
-            .should('not.be.checked')
+        cy.wrap(radioButtons).first().should("not.be.checked");
 
-        cy.wrap(radioButtons)
-            .eq(2)
-            .should('be.disabled')
-    })
+        cy.wrap(radioButtons).eq(2).should("be.disabled");
+      });
+  });
 
-  })
-
-  it('check boxes', () => {
+  it("check boxes", () => {
     cy.visit("/");
     cy.contains("Modal & Overlays").click();
     cy.contains("Toastr").click();
 
-    cy.get('[type="checkbox"]').check({force: true})
-    cy.get('[type="checkbox"]').eq(0).click({force: true})
-  })
+    cy.get('[type="checkbox"]').check({ force: true });
+    cy.get('[type="checkbox"]').eq(0).click({ force: true });
+  });
 
-  it('lists and dropdowns', () => {
-      cy.visit("/");
+  it("lists and dropdowns", () => {
+    cy.visit("/");
 
-      //first option...redundant
+    //first option...redundant
     //   cy.get('nav nb-select').click()
     //   cy.get('.options-list').contains('Dark').click()
     //   cy.get('nav nb-select').should('contain', 'Dark')
@@ -213,111 +223,118 @@ describe("Our first suite", () => {
 
     // Solution number 2 that checks each option and verifies changes
 
-    cy.get('nav nb-select').then( dropdown => {
-        cy.wrap(dropdown).click()
-        cy.get('.options-list nb-option').each( (listItem, index) => {
-            const itemText = listItem.text().trim()
+    cy.get("nav nb-select").then((dropdown) => {
+      cy.wrap(dropdown).click();
+      cy.get(".options-list nb-option").each((listItem, index) => {
+        const itemText = listItem.text().trim();
 
-            const colors = {
-                "Light": "rgb(255, 255, 255)",
-                "Dark": "rgb(34, 43, 69)",
-                "Cosmic": "rgb(50, 50, 89)",
-                "Corporate": "rgb(255, 255, 255)"
-            }
+        const colors = {
+          Light: "rgb(255, 255, 255)",
+          Dark: "rgb(34, 43, 69)",
+          Cosmic: "rgb(50, 50, 89)",
+          Corporate: "rgb(255, 255, 255)",
+        };
 
-            cy.wrap(listItem).click()
-            cy.wrap(dropdown).should('contain', itemText)
-            cy.get('nb-layout-header nav').should('have.css', 'background-color', colors[itemText])
-            if (index < 3){
-                cy.wrap(dropdown).click()
-            }
-        })
-    })
+        cy.wrap(listItem).click();
+        cy.wrap(dropdown).should("contain", itemText);
+        cy.get("nb-layout-header nav").should(
+          "have.css",
+          "background-color",
+          colors[itemText]
+        );
+        if (index < 3) {
+          cy.wrap(dropdown).click();
+        }
+      });
+    });
+  });
 
-
-  })
-
-  it('Web tables', () => {
+  it("Web tables", () => {
     cy.visit("/");
     cy.contains("Tables & Data").click();
     cy.contains("Smart Table").click();
 
     // 1
-    cy.get('tbody').contains('tr', 'Larry').then( tableRow => {
-        cy.wrap(tableRow).find('.nb-edit').click()
-        cy.wrap(tableRow).find('[placeholder="Age"]').clear().type('25')
-        cy.wrap(tableRow).find('.nb-checkmark').click()
-        cy.wrap(tableRow).find('td').eq(6).should('contain', '25')
-    })
+    cy.get("tbody")
+      .contains("tr", "Larry")
+      .then((tableRow) => {
+        cy.wrap(tableRow).find(".nb-edit").click();
+        cy.wrap(tableRow).find('[placeholder="Age"]').clear().type("25");
+        cy.wrap(tableRow).find(".nb-checkmark").click();
+        cy.wrap(tableRow).find("td").eq(6).should("contain", "25");
+      });
 
     // 2
-    cy.get('thead').find('.nb-plus').click()
-    cy.get('thead').find('tr').eq(2).then( tableRow => {
-        cy.wrap(tableRow).find('[placeholder="First Name"]').type('Dominick')
-        cy.wrap(tableRow).find('[placeholder="Last Name"]').type('Spinetti')
-        cy.wrap(tableRow).find('.nb-checkmark').click()
-    })
+    cy.get("thead").find(".nb-plus").click();
+    cy.get("thead")
+      .find("tr")
+      .eq(2)
+      .then((tableRow) => {
+        cy.wrap(tableRow).find('[placeholder="First Name"]').type("Dominick");
+        cy.wrap(tableRow).find('[placeholder="Last Name"]').type("Spinetti");
+        cy.wrap(tableRow).find(".nb-checkmark").click();
+      });
 
-    cy.get('tbody tr').first().find('td').then( tableColumns => {
-        cy.wrap(tableColumns).eq(2).should('contain', 'Dominick')
-        cy.wrap(tableColumns).eq(3).should('contain', 'Spinetti')
-    })
+    cy.get("tbody tr")
+      .first()
+      .find("td")
+      .then((tableColumns) => {
+        cy.wrap(tableColumns).eq(2).should("contain", "Dominick");
+        cy.wrap(tableColumns).eq(3).should("contain", "Spinetti");
+      });
 
-    // 3 
+    // 3
 
-    const age = [20, 30, 40, 200]
+    const age = [20, 30, 40, 200];
 
-    cy.wrap(age).each( age => {
+    cy.wrap(age).each((age) => {
+      cy.get('thead [placeholder="Age"]').clear().type(age);
+      cy.wait(500);
+      cy.get("tbody tr").each((tableRow) => {
+        if (age == 200) {
+          cy.wrap(tableRow).should("contain", "No data found");
+        } else {
+          cy.wrap(tableRow).find("td").eq(6).should("contain", age);
+        }
+      });
+    });
+  });
+  it("tooltip", () => {
+    cy.visit("/");
+    cy.contains("Modal & Overlays").click();
+    cy.contains("Tooltip").click();
 
-        
-            cy.get('thead [placeholder="Age"]').clear().type(age)
-            cy.wait(500)
-            cy.get('tbody tr').each( tableRow => {
-                if(age == 200){
-                    cy.wrap(tableRow).should('contain', 'No data found')
-                } else {
-                cy.wrap(tableRow).find('td').eq(6).should('contain', age)
-                }
-            })
-    })
+    cy.contains("nb-card", "Colored Tooltips").contains("Default").click();
+    cy.get("nb-tooltip").should("contain", "This is a tooltip");
+  });
 
-  })
-  it('lists and dropdowns', () => {
-      
-  })
-  it('tooltip', () => {
-      cy.visit('/')
-      cy.contains('Modal & Overlays').click()
-      cy.contains('Tooltip').click()
-
-    cy.contains('nb-card', 'Colored Tooltips')
-        .contains('Default').click()
-    cy.get('nb-tooltip').should('contain', 'This is a tooltip')
-})
-
-it.only('dialog box', () => {
-    cy.visit('/')
-    cy.contains('Tables & Data').click()
-    cy.contains('Smart Table').click()
+  it("dialog box", () => {
+    cy.visit("/");
+    cy.contains("Tables & Data").click();
+    cy.contains("Smart Table").click();
     //1
-    cy.get('tbody tr').first().find('.nb-trash').click()
-    cy.on('window:confirm', (confirm) => {
-        expect(confirm).to.equal('Are you sure you want to delete?')
-    })
+    cy.get("tbody tr").first().find(".nb-trash").click();
+    cy.on("window:confirm", (confirm) => {
+      expect(confirm).to.equal("Are you sure you want to delete?");
+    });
 
     //2
-    const stub = cy.stub()
-    cy.on('window:confirm', stub)
-    cy.get('tbody tr').first().find('.nb-trash').click().then(() => {
-        expect(stub.getCall(0)).to.be.calledWith('Are you sure you want to delete?')
-    })
+    const stub = cy.stub();
+    cy.on("window:confirm", stub);
+    cy.get("tbody tr")
+      .first()
+      .find(".nb-trash")
+      .click()
+      .then(() => {
+        expect(stub.getCall(0)).to.be.calledWith(
+          "Are you sure you want to delete?"
+        );
+      });
 
     //3
-    cy.get('tbody tr').first().find('.nb-trash').click()
-    cy.on('window:confirm', (confirm) => {
-        expect(confirm).to.equal('Are you sure you want to delete?')
-    })
-        
-})
-
+    cy.get("tbody tr").first().find(".nb-trash").click();
+    cy.on("window:confirm", (confirm) => {
+      expect(confirm).to.equal("Are you sure you want to delete?");
+    });
+  });
 });
